77c8b8986372363fde1ac3666b8d9e570d506802
Mihail14111994 <mihail.plugaru@amdaris.com>
2019-02-15 14:32:08 +0200

    TryCach Unfinished

---
+++ TryCachFinally/TryCachFinally/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCachFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TryCachFinal trycachfinal = new TryCachFinal();
+            trycachfinal.ReadFile(0);
+        }
+    }
+    class TryCachFinal
+    {
+        public void ReadFile(int index)
+        {
+            string path = "test.txt";
+            System.IO.StreamReader file = new System.IO.StreamReader(path);
+            char[] buffer = new char[100];
+            try
+            {
+                file.ReadBlock(buffer, index, buffer.Length);
+            }
+            catch (System.IO.IOException e)
+            {               
+                Console.WriteLine("Error reading from {0}. Message = {1}", path, e.Message);
+            }
+            finally
+            {
+                foreach(var c in buffer)
+                Console.Write(c);
+                if (file != null)
+                {
+                    file.Close();
+                }
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
