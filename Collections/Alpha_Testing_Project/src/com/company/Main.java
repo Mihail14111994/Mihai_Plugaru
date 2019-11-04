8f1d27ee302d6f3f3fb61f59a763161bb7fbb97b
Mihail14111994 <45763627+Mihail14111994@users.noreply.github.com>
2019-11-04 21:43:58 +0200

    Collections task

---
+++ Collections/Alpha_Testing_Project/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String word = new String("acb123123");
+
+        CharList a = new CharList(word);
+        a.mixedList();
+        System.out.println(a);
+
+
+    }
+}
